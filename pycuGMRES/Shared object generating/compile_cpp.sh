060cd3da8285afe7fd9550142b6ef6e9b834809c
archilless <yurii.minin@phystech.edu>
2020-06-18 14:17:37 +0300

    Update to 1.1.3.9.4 version

--- pycuGMRES/Shared object generating/compile_cpp.sh
+++ pycuGMRES/Shared object generating/compile_cpp.sh
@@ -1,16 +1,16 @@
 FOLDER=$1
 soFILE="${FOLDER}/Shared object generating/cuGMRES.so"
+echo importing pycuGMRES 
 
 if test -f "$soFILE"; then
-    rm $soFILE
+    rm "$soFILE"
 fi
-
-echo importing pycuGMRES ...
+...
 echo nvcc -lcublas -lcufft -lcusolver -O3 --compiler-options '-fPIC' -shared \"CUDA\ C\ ++\ sources\" -o cuGMRES.so
-nvcc -lcublas -lcufft -lcusolver -O3 --compiler-options '-fPIC' -shared ${FOLDER}/CUDA\ C\ ++\ sources/TestGMRES.cu -o $soFILE
+nvcc -lcublas -lcufft -lcusolver -O3 --compiler-options '-fPIC' -shared ${FOLDER}/CUDA\ C\ ++\ sources/TestGMRES.cu -o "$soFILE"
 if test -f "$soFILE"; then
-    echo CUDA C++ library has been created!
+    echo pycuGMRES library is ready to use!
 else 
-    echo FATAL ERROR: CUDA C++ library creating failed!
+    echo FATAL ERROR: CUDA C++ - based library creating failed!
 fi
 
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-version = "1.1.3.9.3"
+version = "1.1.3.9.4"
 
 from setuptools import setup
 
