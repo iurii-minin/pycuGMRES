5092956f11d1a456be6928f1e126df38f40bc4a0
archilless <yurii.minin@phystech.edu>
2020-06-23 21:10:13 +0300

    Update to 1.1.4.4.4 version

--- pycuGMRES/CUDA C ++ sources/GMRES_rmk.cuh
+++ pycuGMRES/CUDA C ++ sources/GMRES_rmk.cuh
@@ -19,47 +19,43 @@ void pycuGMRESrmk(
 	char buffer[1024];
 	float tolerance = 0.001f;//0.2f;
 
-	cuComplex **p_h_gamma_arrays = (cuComplex **) malloc((1) * sizeof(cuComplex *));
-
-
-
-	p_h_gamma_arrays[0] = (cuComplex *)malloc((2 * N - 1) * (2 * N - 1) * sizeof(cuComplex));
-
-	sprintf(buffer, "/media/linux/4db3d51d-3503-451d-aff7-07e3ce95927e/Archive/Input/G_prep_%u.txt", N);
-	get_array_C_to_CPU((cuComplex *)p_h_gamma_arrays[0], (const char *)buffer);
-
-
-
 	dim3 blocks(THREADS_PER_BLOCK, THREADS_PER_BLOCK);
 	dim3 threads(Q, Q);
 
-	cuComplex *h_gamma_array = p_h_gamma_arrays[0];
+	cuComplex *h_gamma_array = (cuComplex *)malloc((2 * N - 1) * (2 * N - 1) * sizeof(cuComplex));
+
+	sprintf(buffer, "/media/linux/4db3d51d-3503-451d-aff7-07e3ce95927e/Archive/Input/G_prep_%u.txt", N);
+	get_array_C_to_CPU((cuComplex *)h_gamma_array, (const char *)buffer);
+
+
 
 	cudacall(cudaMemcpy(dev_gamma_array, h_gamma_array, (2 * N - 1) * (2 * N - 1) * sizeof(cuComplex), cudaMemcpyHostToDevice));
 
 	cufftcall(cufftExecC2C(plan, (cuComplex *)dev_gamma_array, (cuComplex *)dev_gamma_array, CUFFT_FORWARD));
 	cudacheckSYN();
 
-		pycuInitSolution((cuComplex *)dev_solution, N);
+	pycuInitSolution((cuComplex *)dev_solution, N);
 
 
-		pycuGMRES(	
-					  (bool *)dev_mask,
-					  (cuComplex *)dev_solution,
-					  for_gradient,
-					  h_index_of_max,
-					  maxiter,
-					  tolerance,
-					  (unsigned int *)GMRES_n,
-					  (float *)dev_actual_residual,
-					  (bool *)h_res_vs_tol_p,
-					  N,
-					  (cuComplex *)dev_gamma_array,
-					  plan,
-					  (cublasHandle_t *)handle_p,
-					  (cusolverDnHandle_t *)cusolverH_p,
-					  (devSubsidiary *)dev_subs,
-					  (timespec *)h_computation_times
-			);
+	pycuGMRES(	
+				  (bool *)dev_mask,
+				  (cuComplex *)dev_solution,
+				  for_gradient,
+				  h_index_of_max,
+				  maxiter,
+				  tolerance,
+				  (unsigned int *)GMRES_n,
+				  (float *)dev_actual_residual,
+				  (bool *)h_res_vs_tol_p,
+				  N,
+				  (cuComplex *)dev_gamma_array,
+				  plan,
+				  (cublasHandle_t *)handle_p,
+				  (cusolverDnHandle_t *)cusolverH_p,
+				  (devSubsidiary *)dev_subs,
+				  (timespec *)h_computation_times
+		);
+
+	free(h_gamma_array);
 
 }
