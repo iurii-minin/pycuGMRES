6b7f8146f0acdf09325d9ded92f3bf3ef2cd274c
archilless <yurii.minin@phystech.edu>
2020-06-25 21:47:17 +0300

    Update to 1.1.4.6.4 version

--- pycuGMRES/CUDA C ++ sources/GMRES_kernels.cuh
+++ pycuGMRES/CUDA C ++ sources/GMRES_kernels.cuh
@@ -197,10 +197,8 @@ __global__ void _2D_to_1D_compared_kernel(	cuComplex *dev_input_mul,  //For GMRE
 		Input_Field.y = - WAVE_NUMBER * (i * cos(ALPHA) + j * sin(ALPHA));
 		Input_Field = my_cexpf(Input_Field);
 		float sigma = N * 0.6;
-		Input_Field.x = Input_Field.x * exp(-pow((float)((float)(j) - N/2.f), 2.f)/pow(sigma, 2.f))/(sigma * sqrt(2.f * 3.14f));
-		Input_Field.y = Input_Field.y * exp(-pow((float)((float)(j) - N/2.f), 2.f)/pow(sigma, 2.f))/(sigma * sqrt(2.f * 3.14f));
-		//Input_Field.x = Input_Field.x * (exp(-pow((float)((float)(j) - 341.f), 2.f)/pow(sigma, 2.f)) + exp(-pow((float)((float)(j) - 682.f), 2.f)/pow(sigma, 2.f)))/(sigma * sqrt(7.28f));
-		//Input_Field.y = Input_Field.y * (exp(-pow((float)((float)(j) - 341.f), 2.f)/pow(sigma, 2.f)) + exp(-pow((float)((float)(j) - 682.f), 2.f)/pow(sigma, 2.f)))/(sigma * sqrt(7.28f));
+		Input_Field.x = Input_Field.x * exp(-pow((float)((float)(j) - (N/2.f - 0.5f)), 2.f)/pow(sigma, 2.f))/(sigma * sqrt(2.f * 3.14f));
+		Input_Field.y = Input_Field.y * exp(-pow((float)((float)(j) - (N/2.f - 0.5f)), 2.f)/pow(sigma, 2.f))/(sigma * sqrt(2.f * 3.14f));
 		Input_Field.x += current_2D.x / ((N << 1) - 1) / ((N << 1) - 1) - arg_old.x;
 		Input_Field.y += current_2D.y / ((N << 1) - 1) / ((N << 1) - 1) - arg_old.y;
 		dev_residual[_1D_index] = Input_Field;
@@ -602,9 +600,7 @@ __global__ void init_x0_kernel(	cuComplex *dev_input,
 	Input_Field.y = - WAVE_NUMBER * (i * cos(ALPHA) + j * sin(ALPHA));
 	Input_Field = my_cexpf(Input_Field);
 	float sigma = N * 0.6;
-	Input_Field.x = Input_Field.x * exp(-pow((float)((float)(j) - N/2.f), 2.f)/pow(sigma, 2.f))/(sigma * sqrt(2.f * 3.14f));
-	Input_Field.y = Input_Field.y * exp(-pow((float)((float)(j) - N/2.f), 2.f)/pow(sigma, 2.f))/(sigma * sqrt(2.f * 3.14f));
-	//Input_Field.x = Input_Field.x * (exp(-pow((float)((float)(j) - 341.f), 2.f)/pow(sigma, 2.f)) + exp(-pow((float)((float)(j) - 682.f), 2.f)/pow(sigma, 2.f)))/(sigma * sqrt(7.28f));
-	//Input_Field.y = Input_Field.y * (exp(-pow((float)((float)(j) - 341.f), 2.f)/pow(sigma, 2.f)) + exp(-pow((float)((float)(j) - 682.f), 2.f)/pow(sigma, 2.f)))/(sigma * sqrt(7.28f));
+	Input_Field.x = Input_Field.x * exp(-pow((float)((float)(j) - (N/2.f - 0.5f)), 2.f)/pow(sigma, 2.f))/(sigma * sqrt(2.f * 3.14f));
+	Input_Field.y = Input_Field.y * exp(-pow((float)((float)(j) - (N/2.f - 0.5f)), 2.f)/pow(sigma, 2.f))/(sigma * sqrt(2.f * 3.14f));
 	dev_input[i * N + j] = Input_Field;
 }
